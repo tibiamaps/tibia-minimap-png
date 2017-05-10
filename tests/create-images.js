1d90721a832daa605ad25a20d1f6ad3e9232da6d
Mathias Bynens <mathias@qiwi.be>
2017-05-10 10:29:22 +0200

    Add more tests

--- tests/create-images.js
+++ tests/create-images.js
@@ -2,9 +2,25 @@ const fs = require('fs');
 
 const { wrapColorData, wrapWaypointData } = require('../index.js');
 
-const buffer = fs.readFileSync('./tests/fixtures/12612507.map');
-const colorData = buffer.slice(0, 0x10000);
-const waypointData = buffer.slice(0x10000, 0x20000);
+const map2png = (from, to) => {
+	const buffer = fs.readFileSync(`./tests/fixtures/${ from }.map`);
+	const colorData = buffer.slice(0, 0x10000);
+	const waypointData = buffer.slice(0x10000, 0x20000);
+	fs.writeFileSync(
+		`./tests/actual/Minimap_Color_${ to }.png`,
+		wrapColorData(colorData)
+	);
+	fs.writeFileSync(
+		`./tests/actual/Minimap_WaypointCost_${ to }.png`,
+		wrapWaypointData(waypointData)
+	);
+};
 
-fs.writeFileSync('./tests/actual/Minimap_Color_32256_32000_7.png', wrapColorData(colorData));
-fs.writeFileSync('./tests/actual/Minimap_WaypointCost_32256_32000_7.png', wrapWaypointData(waypointData));
+const map = new Map([
+	['12612507', '32256_32000_7'],
+	['12612508', '32256_32000_8'],
+]);
+
+for (const [from, to] of map) {
+	map2png(from, to);
+}
